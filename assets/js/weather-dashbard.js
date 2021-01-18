d9b27d89670ac47a68630761fec399af1ec3b4f5
yulduz <yulduz83@gmail.com>
2021-01-17 18:03:11 -0600

    hanles search history on load and on search

--- assets/js/weather-dashbard.js
+++ assets/js/weather-dashbard.js
@@ -4,51 +4,57 @@ var cityCoordinatesList =
   JSON.parse(localStorage.getItem("cityCoordinates")) || [];
 
 var displayWeather = function () {
-  // WHEN I search for a city
-  // THEN I am presented with current and future conditions for that city
-  // and that city is added to the search history
+  // capture user input
+  let cityName = document
+    .querySelector("input.form-control")
+    .value.toLowerCase()
+    .trim();
 
-  //capture user input
-  let cityName = document.querySelector("input.form-control").value.toLowerCase();
+  // get city coordinates
   getCoordinates(cityName).then((response) => {
     let lat = response.coord.lat;
     let lon = response.coord.lon;
     let coordinatesQuery = `lat=${lat}&lon=${lon}`;
-    getWeatherCurrentAndForecast(coordinatesQuery);
+
+    // show city in search history and store coordinates in local storage
+    addCityToSearchHistory(cityName);
     addCityCoordinatesToLocalStorage(cityName, coordinatesQuery);
+
+    // handle city weather details
+    getWeatherCurrentAndForecast(coordinatesQuery).then(() => {
+      // show today's weather details
+      dispayTodaysWeather();
+
+      // show 5 day weather forcast
+      displayFiveDayForecast();
+    });
   });
 };
 
 //**************** API CALLS ****************
 
 //given user unput, make API call to further retrieve coordinates
-var getCoordinates = function (cityName) {
-  return fetch(
+var getCoordinates = async function (cityName) {
+  const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
-  ).then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    // generic msg that handles any non 200 reponse
-    else alert(
-      `Sorry we the city you provided as "${cityName.toUpperCase()}" was not found, please try again`
-    );
-  });
+  );
+  if (response.ok) {
+    return response.json();
+  }
+  // generic msg that handles any non 200 reponse
+  else alert(`Could not find "${cityName.toUpperCase()}", please try again`);
 };
 
 //given the coordinates, make API call for current and future weather conditions
-var getWeatherCurrentAndForecast = function (coordinatesQuery) {
-  //lat=30.2672&lon=-97.7431
-  //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-  return fetch(
+var getWeatherCurrentAndForecast = async function (coordinatesQuery) {
+  const response = await fetch(
     `https://api.openweathermap.org/data/2.5/onecall?${coordinatesQuery}&appid=${apiKey}`
-  ).then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    // generic msg that handles any non 200 reponse
-    else alert(`Something went wrong, please try again later.`);
-  });
+  );
+  if (response.ok) {
+    return response.json();
+  }
+  // generic msg that handles any non 200 reponse
+  else alert(`Something went wrong, please try again later.`);
   //TODO: Error handlers
 };
 
@@ -73,4 +79,53 @@ var addCityCoordinatesToLocalStorage = function (cityName, coordinatesQuery) {
 
 //**************** DOM MANIPULATION ****************
 
+// helper util which attaches the city to search history
+var attachCityInfoToSearchHistory = function (cityName) {
+    var cityListEl = $(".search-history");
+    var cityListItemEl = document.createElement("div");
+    cityListItemEl.classList.add("list-item");
+    const cityNameCapitalized =
+      cityName.charAt(0).toUpperCase() + cityName.slice(1);
+  
+    cityListItemEl.textContent = cityNameCapitalized;
+    cityListEl.append(cityListItemEl);
+  };
+
+// show search history on page load
+var loadCitySearchHistory = function () {
+  for (var i = 0; i < cityCoordinatesList.length; i++) {
+    attachCityInfoToSearchHistory(cityCoordinatesList[i].city)
+    // var cityListEl = $(".search-history");
+    // var cityListItemEl = document.createElement("div");
+    // cityListItemEl.classList.add("list-item");
+
+    // // retrieve the cityname from the storage
+    // var cityName = cityCoordinatesList[i].city;
+    // // capitalize first letter before displaying
+    // const cityNameCapitalized =
+    //   cityName.charAt(0).toUpperCase() + cityName.slice(1);
+
+    // cityListItemEl.textContent = cityNameCapitalized;
+    // cityListEl.append(cityListItemEl);
+  }
+};
+
+// add city to search history (only if unique)
+var addCityToSearchHistory = function (cityName) {
+  // add city to the list only if distinct
+  for (var i = 0; i < cityCoordinatesList.length; i++) {
+    if (cityCoordinatesList[i].city === cityName) {
+      return;
+    }
+  }
+  attachCityInfoToSearchHistory(cityName);
+};
+
+var dispayTodaysWeather = function () {};
+
+var displayFiveDayForecast = function () {};
+
 //**************** LISTENERS ****************
+
+//**************** ON LOAD ****************
+loadCitySearchHistory();
